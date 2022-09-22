9a861309696443de5ec6e5e33731aa6db40b186d
lucas <fernandes20lucas@gmail.com>
2022-09-21 22:20:05 -0300

    feat: findUserById middleware

--- src/index.js
+++ src/index.js
@@ -55,7 +55,14 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) return response.status(404).json({ error: 'User not fount' });
+
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
